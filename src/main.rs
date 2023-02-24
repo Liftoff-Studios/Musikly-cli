331d7027b4fa51259a7147215dbff42305e9b4e1
Abhijit Biswas <abhijitbiswas07@gmail.com>
2023-02-24 10:22:09 +0530

    foo

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,19 @@ use rand::thread_rng;
 use rand::seq::SliceRandom;
 
 fn main(){
+    let mut dir_name = String::new();
+    let pattern = std::env::args().nth(1);
+    match pattern{
+        Some(x) => dir_name = String::from("Music")+&x,
+        None => dir_name = String::from("Music")
+    }
+
+    // Music Part
     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
     let mut paths = vec![];
 
-    for entry in fs::read_dir("Music").unwrap() {
+    for entry in fs::read_dir(dir_name.as_str()).unwrap() {
         let dir = entry.unwrap();
         paths.push(dir.path())
     }
